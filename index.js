860a9b195273e1ddbf48e9ca5c8556654113a459
Youssef Al Hamadi <yuosef.smh@hotmail.com>
2024-10-11 15:34:15 +0400

    Initial Commit

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const multer = require('multer');
 var app = express();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({storage: storage});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +22,16 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if(!req.file) {
+    return res.json({error: "No file uploaded"});
+  }
+  res.json({
+    name: req.file.filename,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+})
 
 
 const port = process.env.PORT || 3000;
